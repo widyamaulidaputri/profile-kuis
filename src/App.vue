3742ac3958b201fefb77d702a4b8b0a19d05a90b
Widya Maulida Putri <12150124092@students.uin-suska.ac.id>
2023-04-12 18:34:41 +0700

    this-new

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">PROFILE | Widya Maulida P</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <div class="mx-auto"></div>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#"><router-link to="/" style="color: black;">Home</router-link></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#"><router-link to="/about" style="color: black;">About</router-link></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+
+  <router-view />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
 
 <style>
-#app {
+.navbar-expand-lg {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -23,4 +43,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <!--banner image-->
+
+  <div class="banner-image w-100 vh-100 d-flex justify-content-center align-items-center">
+    <div class="content text-center">
+      <h1 class="text-white">Selamat Datang Diwebsiteku!</h1>
+    </div>
+  </div>
+
+
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+.banner-image{
+  background: green;
+}
+
+</style>
