d01d5f7b89c0d693cd444896df76c4d64c054bd2
widyamaulidaputri <129193189+widyamaulidaputri@users.noreply.github.com>
2023-04-16 23:59:55 +0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createWebHistory, createRouter } from "vue-router";
 import Home from "@/views/Home.vue";
 import About from "@/views/About.vue";
+import Album from "@/views/Album.vue";
 
 const routes = [
     {
@@ -12,6 +13,11 @@ const routes = [
         path: "/about",
         name: "AboutView",
         component: About,
+    },
+    {
+        path: "/album",
+        name: "AlbumView",
+        component: Album,
     },
 ];
 
