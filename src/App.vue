c403422738118985b9dc8eb1f4147eec888dee92
widyamaulidaputri <129193189+widyamaulidaputri@users.noreply.github.com>
2023-04-16 23:58:28 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">PROFILE | Widya Maulida P</a>
+      <a class="navbar-brand" href="#">PROFILE | Widya Maulida Putri</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
+
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <div class="mx-auto"></div>
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#"><router-link to="/" style="color: black;">Home</router-link></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#"><router-link to="/about" style="color: black;">About</router-link></a>
-          </li>
+
+        <ul class="nav-link">
+          <li><a href="" class="active"><router-link to="/" style="color: black;">Home</router-link></a></li>
+          <li><a href="" class="active"><router-link to="/about" style="color: black;">About</router-link></a></li>
+          <li><a href="" class="active"><router-link to="/album" style="color: #06060b;">Album</router-link></a></li>
+
+
         </ul>
       </div>
     </div>
@@ -40,8 +41,38 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  background-color: rgba(171, 102, 102, 0.62);
+  color: white;
   margin-top: 60px;
-}
+  display: flex;
+  justify-content: space-between;
+  padding: 5px 30px ;
 
+}
+.nav-link{
+  display: flex;
+  align-items: center;
+
+}
+.nav-link li{
+  list-style: none;
+  margin-left: 20px;
+  align-items: center;
+  float: left;
+}
+.nav-link li a{
+  text-decoration: none;
+  color: white;
+  font-weight: 500;
+}
+.active{
+  border-bottom: 2px solid #8d7e7f;
+  padding: 5px;
+}
+.nav-link li a:hover {
+  color: red;
+  font-size: 20px;
+  transition: .2s;
+}
 </style>
+
