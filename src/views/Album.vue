2f82b3ab4f781d10988ffd4c9231a0302348be27
widyamaulidaputri <129193189+widyamaulidaputri@users.noreply.github.com>
2023-04-17 00:09:09 +0700

    Update Album.vue

---
+++ src/views/Album.vue
@@ -0,0 +1,58 @@
+<template>
+  <section id = "sec-7" class = "py-5">
+
+    <div class = "container my-4">
+      <div class = "row gx-md-5">
+        <div class = "col-md-6 my-3">
+          <div class = "card text-center">
+            <img class = "card-img-top" src = "../assets/deyabayi.jpeg">
+            <div class = "card-body py-5">
+              <h4 class = "card-title">Masa Kecil</h4>
+              <p class = "card-text py-3 lh-lg text-muted fw-light">Foto diatas ketika saya berumur 1 tahun. Disaat itu sedang ada acara kumpul keluarga dan yang mengambil foto tersebut adalah oom saya.</p>
+            </div>
+          </div>
+        </div>
+
+        <div class = "col-md-6 my-3">
+          <div class = "card text-center">
+            <img class = "card-img-top" src = "../assets/deyatk1.jpeg">
+            <div class = "card-body py-5">
+              <h4 class = "card-title">Perpisahan TK Pembina</h4>
+              <p class = "card-text py-3 lh-lg text-muted fw-light">Foto diatas diambil ketika perpisahan TK pembina dan saya terpilih menjadi mayoret pada saat itu. </p>
+            </div>
+          </div>
+        </div>
+
+        <div class = "col-md-6 my-3">
+          <div class = "card text-center">
+            <img class = "card-img-top" src = "../assets/deyatk.jpeg">
+            <div class = "card-body py-5">
+              <h4 class = "card-title">XII MIPA 6</h4>
+              <p class = "card-text py-3 lh-lg text-muted fw-light">Foto diatas merupakan foto saya di masa SMA. Foto tersebut menjadi kenang-kenangan terakhir di masa SMA karena pengambilan foto tersebut dihari perpisahan sekolah.</p>
+            </div>
+          </div>
+        </div>
+
+        <div class = "col-md-6 my-3">
+          <div class = "card text-center">
+            <img class = "card-img-top" src = "../assets/deyakuliah.jpeg">
+            <div class = "card-body py-5">
+              <h4 class = "card-title">Type-C</h4>
+              <p class = "card-text py-3 lh-lg text-muted fw-light">Foto diatas merupakan foto di Masa Kini. Sekarang saya berkuliah di UIN SUSKA RIAU Jurusan Teknik Informatika dan berada di kelas C pada tahun ke dua. </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "App"
+}
+</script>
+
+<style scoped>
+
+</style>
